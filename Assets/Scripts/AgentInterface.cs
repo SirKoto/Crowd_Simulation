7851330f742b67b46ad273f6f1c1ba532b2f96c0
Pol Martín Garcia <pol147258369@gmail.com>
2021-11-27 11:05:56 +0100

    Update unity version. Finish scene 1

--- Assets/Scripts/AgentInterface.cs
+++ Assets/Scripts/AgentInterface.cs
@@ -7,22 +7,11 @@ public class AgentInterface : MonoBehaviour
 {
     private Animator m_animat;
 
+    private Rigidbody m_rigidbody;
 
-    public void set_speed_local(Vector2 vel)
-    {
-        float v = vel.x;
-        float h = vel.y;
-        m_animat.SetFloat("speedX", v);
-        m_animat.SetFloat("speedZ", h);
-    }
 
-    public void set_speed_world(Vector2 vel)
-    {
-        Vector3 n_vel = new Vector3(vel.x, 0.0f, vel.y);
-        n_vel = gameObject.transform.InverseTransformVector(n_vel);
-
-        this.set_speed_local(new Vector2(n_vel.x, n_vel.z));
-    }
+    private Vector3 m_prev_position;
+    private Vector3 m_velocity = Vector3.zero;
 
     public void look_at(Vector2 dir)
     {
@@ -30,10 +19,73 @@ public class AgentInterface : MonoBehaviour
         gameObject.transform.forward = new_dir.normalized;
     }
 
-    // Start is called before the first frame update
+    public void set_velocity(Vector2 vel)
+    {
+        Vector3 vel3 = new Vector3(vel.x, 0.0f, vel.y);
+
+        if (!vel3.Equals(Vector3.zero)) {
+            //m_rigidbody.AddForce(50 * vel3, ForceMode.Acceleration);
+
+            m_rigidbody.MovePosition(gameObject.transform.position + Time.fixedDeltaTime * vel3);
+        }
+    }
+
+    private void set_speed_local_animation(Vector2 vel)
+    {
+        float v = -vel.x;
+        float h = vel.y;
+        m_animat.SetFloat("speedX", h);
+        m_animat.SetFloat("speedZ", v);
+    }
+
+    private void set_speed_world_animation(Vector2 vel)
+    {
+        Vector3 n_vel = new Vector3(vel.x, 0.0f, vel.y);
+        n_vel = gameObject.transform.InverseTransformVector(n_vel);
+
+        this.set_speed_local_animation(new Vector2(n_vel.x, n_vel.z));
+    }
+
+    private void set_speed_world_animation(Vector3 vel)
+    {
+        Vector3 obj_vel = gameObject.transform.InverseTransformVector(vel);
+
+        this.set_speed_local_animation(new Vector2(obj_vel.x, obj_vel.z));
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 delta_pos = m_rigidbody.transform.position - m_prev_position;
+        m_velocity = delta_pos / Time.deltaTime;
+
+        set_speed_world_animation(m_velocity);
+
+        m_prev_position = m_rigidbody.transform.position;
+
+    }
+
     void Start()
     {
+        m_rigidbody = gameObject.GetComponent<Rigidbody>();
+        if (m_rigidbody == null)
+        {
+            Debug.LogError("Can't find Rigidbody");
+        }
+
         m_animat = gameObject.GetComponent<Animator>();
+        if (m_animat == null)
+        {
+            Debug.LogError("Can't find Animator");
+        }
+
+        m_prev_position = gameObject.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+
+ 
     }
 
 }
--- Assets/Scripts/BasicAnimatorControllerScript.cs
+++ Assets/Scripts/BasicAnimatorControllerScript.cs
@@ -6,24 +6,28 @@ using System;
 public class BasicAnimatorControllerScript : MonoBehaviour
 {
 
+
+    [SerializeField]
     private AgentInterface m_interface;
+
+    [SerializeField]
+    private float m_scale = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_interface = gameObject.GetComponent<AgentInterface>();
+        //m_interface = gameObject.GetComponent<AgentInterface>();
         if(m_interface == null)
         {
             Debug.LogError("Can't find Agent Interface");
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float h = -2.0f * Input.GetAxis("Horizontal");
-        float v = 2.0f * Input.GetAxis("Vertical");
-        m_interface.set_speed_world(new Vector2(v, h));
+        float h = m_scale * Input.GetAxis("Horizontal");
+        float v = m_scale * Input.GetAxis("Vertical");
+        m_interface.set_velocity(new Vector2(h, v));
 
         //m_interface.look_at(new Vector2(v, h));
     }
