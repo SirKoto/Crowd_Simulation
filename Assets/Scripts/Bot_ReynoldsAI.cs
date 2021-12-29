f22a991c1b6fb266d548158efc2706779c817eb2
Pol Martín Garcia <pol147258369@gmail.com>
2021-12-29 20:37:38 +0100

    Exercise 4. Collision avoidance

---
+++ Assets/Scripts/Bot_ReynoldsAI.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot_ReynoldsAI : MonoBehaviour
+{
+
+    private Vector2 m_init = Vector2.zero;
+    private Vector2 m_goal = Vector2.zero;
+    private Vector2 m_direction = Vector2.zero;
+    private float m_max_speed = 3.0f;
+    private float m_min_speed = 1.2f;
+    private float m_speed = 2.0f;
+
+    private AgentInterface m_agent = null;
+    private Exercise_4_Manager m_manager = null;
+    private List<Vector2> m_goal_queue = new List<Vector2>();
+    private int m_goal_idx = 0;
+
+    private Coroutine m_rotate_coroutine = null;
+    private Coroutine m_direction_coroutine = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_agent = gameObject.GetComponent<AgentInterface>();
+        m_manager = gameObject.GetComponentInParent<Exercise_4_Manager>();
+        m_speed = Random.Range(m_min_speed, m_max_speed);
+        setup_goal_and_route();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 collision_avoidance3 = m_manager.get_collision_avoidance(gameObject.transform.position);
+        Vector2 collision_avoidance = new Vector2(collision_avoidance3.x, collision_avoidance3.z);
+        Vector2 forward = new Vector2(transform.forward.x, transform.forward.z);
+        float dot = Vector2.Dot(forward, collision_avoidance);
+        collision_avoidance = collision_avoidance - dot * forward;
+
+        m_agent.set_velocity((m_direction +
+            collision_avoidance
+            ) * m_speed);
+
+
+        if (is_goal_close() || !is_correct_direction())
+        {
+            setup_goal_and_route();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(gameObject.transform.position, new Vector3(m_goal.x, 0.0f, m_goal.y));
+
+        Gizmos.color = Color.green;
+        for (int i = Mathf.Max(m_goal_idx - 1, 0); i < m_goal_queue.Count - 1; ++i)
+        {
+            Vector3 from = new Vector3(m_goal_queue[i].x, 0.0f, m_goal_queue[i].y);
+            Vector3 to = new Vector3(m_goal_queue[i + 1].x, 0.0f, m_goal_queue[i + 1].y);
+            Gizmos.DrawLine(from, to);
+        }
+
+        Gizmos.color = Color.red;
+        Vector3 collision_avoidance3 = m_manager.get_collision_avoidance(gameObject.transform.position);
+        Gizmos.DrawLine(transform.position, transform.position + collision_avoidance3);
+    }
+
+    private bool is_goal_close()
+    {
+        return Vector2.SqrMagnitude((new Vector2(transform.position.x, transform.position.z)) - m_goal) < 0.1f;
+    }
+
+    private bool is_correct_direction()
+    {
+        // Because the path regenerates each time
+        // We do not need to check if the direction is correct
+        return true;
+    }
+
+    public void regenerate_A_star()
+    {
+        m_init = new Vector2(transform.position.x, transform.position.z);
+        Vector2 offset = new Vector2(m_manager.get_domain_half_size(), m_manager.get_domain_half_size());
+        Vector2 offset05 = new Vector2(m_manager.get_domain_half_size() - 0.5f, m_manager.get_domain_half_size() - 0.5f);
+
+        m_goal_idx = 0;
+        m_goal = m_goal_queue[m_goal_queue.Count - 1];
+        m_goal_queue = m_manager.m_grid.get_path_to_bidirectional(m_init + offset, m_goal + offset05);
+
+        for (int i = 0; i < m_goal_queue.Count; ++i)
+        {
+            m_goal_queue[i] -= offset05;
+        }
+
+        if (m_goal_queue.Count > 0)
+        {
+            m_goal = m_goal_queue[m_goal_idx++];
+        }
+        else
+        {
+            m_direction = Vector2.zero;
+            m_goal = m_init;
+        }
+
+        if (m_direction_coroutine != null)
+        {
+            StopCoroutine(m_direction_coroutine);
+        }
+        m_direction_coroutine = StartCoroutine(smooth_change_direction(0.2f));
+    }
+
+    private void setup_goal_and_route()
+    {
+        m_init = new Vector2(transform.position.x, transform.position.z);
+
+        if (m_goal_queue.Count > m_goal_idx)
+        {
+            m_goal = m_goal_queue[m_goal_idx++];
+        }
+        else
+        {
+            Vector2 offset = new Vector2(m_manager.get_domain_half_size(), m_manager.get_domain_half_size());
+            m_goal_idx = 0;
+            m_goal = m_manager.get_random_empty_pos_0_size();
+            m_goal_queue = m_manager.m_grid.get_path_to_bidirectional(m_init + offset, m_goal);
+
+            offset = new Vector2(m_manager.get_domain_half_size() - 0.5f, m_manager.get_domain_half_size() - 0.5f);
+            for (int i = 0; i < m_goal_queue.Count; ++i)
+            {
+                m_goal_queue[i] -= offset;
+            }
+
+            if (m_goal_queue.Count > 0)
+            {
+                m_goal = m_goal_queue[m_goal_idx++];
+            }
+            else
+            {
+                m_goal = m_init;
+            }
+        }
+
+        if (m_direction_coroutine != null)
+        {
+            StopCoroutine(m_direction_coroutine);
+        }
+        m_direction_coroutine = StartCoroutine(smooth_change_direction(0.2f));
+    }
+
+    private IEnumerator rotate_agent(Vector2 look_to, float duration)
+    {
+        Quaternion ini = gameObject.transform.rotation;
+        Quaternion end = Quaternion.LookRotation(new Vector3(look_to.x, 0.0f, look_to.y));
+
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            Quaternion interp = Quaternion.Slerp(ini, end, t);
+            gameObject.transform.rotation = interp;
+            t += Time.deltaTime / duration;
+            yield return null;
+        }
+
+        gameObject.transform.rotation = end;
+    }
+
+    private IEnumerator smooth_change_direction(float duration)
+    {
+        Vector2 start_dir = m_direction;
+        Vector2 end_dir = (m_goal - m_init).normalized;
+
+        if (m_rotate_coroutine != null)
+        {
+            StopCoroutine(m_rotate_coroutine);
+        }
+        m_rotate_coroutine = StartCoroutine(rotate_agent(end_dir, 1.0f));
+
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            m_direction = Vector2.Lerp(start_dir, end_dir, t);
+            t += Time.deltaTime / duration;
+            yield return null;
+        }
+
+        m_direction = end_dir;
+    }
+}
---
+++ Assets/Scripts/Exercise_4_Manager.cs
@@ -0,0 +1,200 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise_4_Manager : MonoBehaviour
+{
+
+    [SerializeField]
+    private uint m_domain_size = 20;
+    [SerializeField]
+    private uint m_num_agents = 20;
+
+    [SerializeField]
+    private GameObject m_floor = null;
+
+    [SerializeField]
+    private GameObject m_gameObject_to_instance = null;
+    [SerializeField]
+    private GameObject m_wall_gameObject = null;
+
+    [SerializeField]
+    private float m_collision_avoidance_radius = 1.0f;
+    [SerializeField]
+    private float m_collision_avoidance_strength = 0.6f;
+
+    public A_Grid m_grid;
+
+    private List<GameObject> m_walls = new List<GameObject>();
+    private List<GameObject> m_agents = new List<GameObject>();
+
+    public float get_domain_half_size()
+    {
+        return (float)m_domain_size / 2.0f;
+    }
+
+
+    public Vector2 get_random_empty_pos_0_size()
+    {
+        Vector2Int pos;
+        do
+        {
+            pos = new Vector2Int(Random.Range(0, (int)m_domain_size), Random.Range(0, (int)m_domain_size));
+        } while (m_grid.is_obstacle(pos));
+
+        Vector2 finalPos = pos + new Vector2(Random.Range(0.2f, 0.8f), Random.Range(0.2f, 0.8f));
+        return finalPos;
+    }
+
+    public Vector2 get_random_empty_pos()
+    {
+        Vector2Int pos;
+        do
+        {
+            pos = new Vector2Int(Random.Range(0, (int)m_domain_size), Random.Range(0, (int)m_domain_size));
+        } while (m_grid.is_obstacle(pos));
+
+        Vector2 finalPos = pos + new Vector2(Random.Range(0.2f, 0.8f) - get_domain_half_size(), Random.Range(0.2f, 0.8f) - get_domain_half_size());
+
+        if (finalPos.x < -m_domain_size || finalPos.x > m_domain_size)
+        {
+            Debug.LogError("Error: bad position x " + finalPos.x.ToString());
+        }
+
+        if (finalPos.y < -m_domain_size || finalPos.y > m_domain_size)
+        {
+            Debug.LogError("Error: bad position y " + finalPos.y.ToString());
+        }
+
+        return finalPos;
+    }
+
+    public Vector3 get_collision_avoidance(Vector3 pos)
+    {
+        Vector3 dir = Vector3.zero;
+        float radius = Mathf.Sqrt(2.0f * 0.5f * 0.5f) + m_collision_avoidance_radius;
+
+        float sum_weight = 0.0f;
+        foreach (GameObject obj in m_walls)
+        {
+            Vector3 delta = pos - obj.transform.position;
+            float sq_dist_wall = Vector2.SqrMagnitude(new Vector2(delta.x - 0.5f, delta.z - 0.5f));
+            if(sq_dist_wall < radius * radius)
+            {
+                float dist = Mathf.Sqrt(sq_dist_wall);
+                float t = 1.0f - (dist - Mathf.Sqrt(2.0f * 0.5f * 0.5f)) / m_collision_avoidance_radius;
+                float w = t * t;
+                sum_weight += w;
+                dir += w * delta.normalized;
+            }
+        }
+
+        if(sum_weight <= 1e-4f)
+        {
+            return Vector3.zero;
+        }
+
+        return m_collision_avoidance_strength / sum_weight * dir;
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.cyan;
+        float radius = Mathf.Sqrt(2.0f * 0.5f * 0.5f) + m_collision_avoidance_radius;
+        foreach (GameObject obj in m_walls)
+        {
+            Gizmos.DrawWireSphere(obj.transform.position + new Vector3(0.5f, 0.0f, 0.5f), radius);
+        }
+    }
+
+    private IEnumerator regenerate_A_star()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.5f);
+
+            m_grid.reset_penalizations();
+
+            foreach (GameObject obj in m_agents)
+            {
+                Vector3 pos3 = obj.transform.position;
+                Vector2Int pos2 = Vector2Int.FloorToInt(new Vector2(pos3.x + get_domain_half_size(), pos3.z + get_domain_half_size()));
+                m_grid.add_penalization(pos2);
+            }
+
+            foreach (GameObject obj in m_agents)
+            {
+                obj.GetComponent<Bot_ReynoldsAI>().regenerate_A_star();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_floor == null)
+        {
+            Debug.LogError("Floor not assigned");
+        }
+
+        float plane_size = m_floor.GetComponent<MeshCollider>().bounds.size.x;
+        float scale = (float)(m_domain_size) / plane_size;
+        m_floor.transform.localScale = new Vector3(scale, 1.0f, scale);
+
+        if (m_gameObject_to_instance == null)
+        {
+            Debug.LogError("Game object to instance not assigned");
+        }
+
+        if (m_wall_gameObject == null)
+        {
+            Debug.LogError("Wall Game Object not assigned");
+        }
+
+        m_grid = new A_Grid(m_domain_size);
+
+        // Instantiate walls
+        Vector3 offset = new Vector3(get_domain_half_size(), 0.0f, get_domain_half_size());
+        Random.InitState(15);
+        for (int i = 0; i < m_domain_size; ++i)
+        {
+            for (int j = 0; j < m_domain_size; ++j)
+            {
+                if (Random.value < 0.1f)
+                {
+                    m_grid.add_obstacle(new Vector2Int(i, j));
+                    Vector3 pos = new Vector3(i, 0.0f, j) - offset;
+                    GameObject obj = GameObject.Instantiate(m_wall_gameObject, pos, Quaternion.identity, gameObject.transform);
+                    m_walls.Add(obj);
+                }
+            }
+        }
+
+        // Instantiate agents
+
+        GameObject duplicate = GameObject.Instantiate(m_gameObject_to_instance, Vector3.zero, Quaternion.identity, gameObject.transform);
+
+
+        duplicate.AddComponent<Bot_ReynoldsAI>();
+
+        for (uint i = 0; i < m_num_agents; ++i)
+        {
+            Vector2 pos2 = get_random_empty_pos();
+            Vector3 pos = new Vector3(pos2.x, 0.0f, pos2.y);
+            GameObject obj = GameObject.Instantiate(duplicate, pos, Quaternion.identity, gameObject.transform);
+            m_agents.Add(obj);
+        }
+
+        GameObject.Destroy(duplicate);
+
+        // Start regeneration courutines
+        StartCoroutine(regenerate_A_star());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
